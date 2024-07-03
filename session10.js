8d60ab0b9110601054717377105f9c91756a3961
hungdz2k1 <nguyenphuonghung1002@gmail.com>
2024-07-03 15:24:22 +0700

    update

---
+++ session10.js
@@ -0,0 +1,40 @@
+
+
+// let number1 = parseInt(prompt("nhập hộ số đê bạn ê"))
+
+
+// let number2 = parseInt(prompt("nhập hộ số đê bạn ê"))
+
+
+
+// alert((number1 + number2))
+
+
+
+// let myName ="ハローワールド. \n マイ　ネーム　イズ　フン"
+
+// let stringD ="đầu cắt moi"
+
+
+
+// let stringC =` vl luôn ${stringD} `
+
+
+// console.log(stringC)
+
+
+
+let result = a > b ? "thi a lon hon b" : " a be hon b"
+alert(result)
+
+// Điều Kiện ? nếu điều kiện đúng : nếu điều kiện sai 
+
+
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+//index vi tri
+// 
+
